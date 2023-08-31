8ab8c8258c74136e2a7c704fb816c6f65e844196
GustavoRBecker <gustavorosabecker@gmail.com>
2023-08-31 19:07:49 -0300

    changing to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Pokepage from './pages/pokedex/pokepage';
 import Pokedex from './pages/home';
 
 function App() {
   return (
-    <BrowserRouter basename= "/hello-world">
+    <HashRouter>
       <Routes>
-        <Route exact path='/hello-world' element={<Pokedex />}/>
-        <Route path='/hello-world/pokepage/:nomePokemon' element={<Pokepage />}/>
+        <Route path='/' element={<Pokedex />}/>
+        <Route path='/pokepage/:nomePokemon' element={<Pokepage />}/>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 

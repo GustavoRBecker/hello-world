2f7c499b6d90e471792ff6abbb6ad521106b1372
GustavoRBecker <gustavorosabecker@gmail.com>
2023-09-01 10:10:25 -0300

    adicionando botões para gerações de Pokémon

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -12,20 +12,54 @@ function Pokedex() {
             .catch(error => console.error('Error fetching data: ', error));
     }, []);
 
+    let gen1 = nomesPokemon.slice(0, 151)
+    let gen2 = nomesPokemon.slice(151, 251)
+    let gen3 = nomesPokemon.slice(251, 386)
+    const [selecionaGeracao, setSelecionaGeracao] = useState(gen1)
+
     return (
         <div className="pokeList-container">
             <div className="pokeList-content">
                 <h1>Pokélist</h1>
                 <div className="nameList">
                     <ul>
-                        {nomesPokemon.map((pokemon) => (
-                            <li className="pokeLink" key={pokemon.name}>
-                                <Link className="pokeLink" to={`/pokepage/${pokemon.name}`}>
-                                    {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
-                                </ Link>
-                            </li>
-                        ))}
+                        {nomesPokemon ? (
+                            <>
+                                {selecionaGeracao.map((pokemon) => (
+                                    <li className="pokeLink" key={pokemon.name}>
+                                        <Link className="pokeLink" to={`/pokepage/${pokemon.name}`}>
+                                            {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
+                                        </ Link>
+                                    </li>
+                                ))}
+                            </>
+                        ) : (
+                            <div className="loading-container">
+                                <div className="loading-gif">
+                                </div>
+                            </div>
+                        )}
                     </ul>
+                </div>
+                <div className="generation-container">
+                    <button
+                        className= {"changer" + selecionaGeracao === 1 ? 'ativa' : ''}
+                        onClick={() => setSelecionaGeracao(gen1)}
+                    >
+                        Gen 1
+                    </button>
+                    <button
+                        className= {"changer" + selecionaGeracao === 2 ? 'ativa' : ''}
+                        onClick={() => setSelecionaGeracao(gen2)}
+                    >
+                        Gen 2
+                    </button>
+                    <button
+                        className= {"changer" + selecionaGeracao === 3 ? 'ativa' : ''}
+                        onClick={() => setSelecionaGeracao(gen3)}
+                    >
+                        Gen 3
+                    </button>
                 </div>
             </div>
         </div>
